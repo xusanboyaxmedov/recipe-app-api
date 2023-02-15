4239801edae73a81f3ced4bc6d1b171a8e49b7e5
XusanboyAxmedov <khusanboyakhmedov2805@gmail.com>
2023-02-15 19:52:08 +0500

    Configured docker compose and wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -19,4 +19,4 @@ class CalcTests(SimpleTestCase):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
-        self.assertEqual(res, 5)                         
+        self.assertEqual(res, 5) 
